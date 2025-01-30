e6db961c2c1c3c2d3e75e7d6a8aa47d1ea088f1a
Abdellah Ramadan <ramadanabdel24@gmail.com>
2025-01-30 22:29:40 +0100

    add some little tweaks

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -17,10 +17,6 @@ class _HomeState extends State<Home> {
   final database = MyDatabase();
   int todoCount = TodoProvider().count;
 
-  Future<List<Task>> _getTasks() async {
-    return await database.tasks();
-  }
-
   @override
   void initState() {
     context.read<TodoProvider>().getDoneTaks();
@@ -42,72 +38,86 @@ class _HomeState extends State<Home> {
                   ],
                 ),
             child: null),
-        Consumer<TodoProvider>(
-          builder: (BuildContext context, todoProvider, Widget? child) =>
-              Expanded(
-            child: FutureBuilder<List<Task>>(
-              future: context.read<TodoProvider>().getTasks(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return CircularProgressIndicator();
-                  case ConnectionState.done:
-                    var data = snapshot.data;
-                    if (snapshot.hasData) {
-                      return snapshot.data!.isEmpty
-                          ? Text('no data yet')
-                          : ListView.builder(
-                              itemCount: snapshot.data!.length,
-                              shrinkWrap: true,
-                              physics: NeverScrollableScrollPhysics(),
-                              itemBuilder: (context, index) => Card(
-                                margin: EdgeInsets.all(10.0),
-                                child: ListTile(
-                                  leading: Checkbox(
-                                      value: data![index].isDone == 0
-                                          ? false
-                                          : true,
-                                      onChanged: (value) async {
-                                        if (value == false) {
-                                          context
-                                              .read<TodoProvider>()
-                                              .markUnDone(data[index].id ?? 0);
-                                        }
-
-                                        if (value == true) {
-                                          context
-                                              .read<TodoProvider>()
-                                              .markDone(data[index].id ?? 0);
-                                        }
-                                      }),
-                                  title: Text(snapshot.data![index].title),
-                                  subtitle:
-                                      Text(snapshot.data![index].description),
-                                  onTap: () {
-                                    int id = data[index].id ?? 0;
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                SingleTask(id: id)));
-                                  },
-                                ),
+        Expanded(
+          child: FutureBuilder<List<Task>>(
+            future: context.read<TodoProvider>().getTasks(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return CircularProgressIndicator();
+                case ConnectionState.done:
+                  var data = snapshot.data;
+                  if (snapshot.hasData) {
+                    return snapshot.data!.isEmpty
+                        ? Text('no data yet')
+                        : ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            shrinkWrap: true,
+                            physics: AlwaysScrollableScrollPhysics(),
+                            itemBuilder: (context, index) => Dismissible(
+                              key: Key(data![index].title),
+                              onDismissed: (direction) async {
+                                late var id = data[index].id;
+                                await context
+                                    .read<TodoProvider>()
+                                    .deleteTask(id!);
+                              },
+                              background: Container(
+                                color: Colors.redAccent,
                               ),
-                            );
-                    }
-                    if (snapshot.hasError) {
-                      return Text('There is an error ${snapshot.error}');
-                    }
-                    return Text('Just here');
-                  default:
-                    return ErrorWidget(
-                        Exception('An unexpected error occured'));
-                }
-              },
-            ),
+                              child: Consumer<TodoProvider>(
+                                builder: (BuildContext context, todo,
+                                        Widget? child) =>
+                                    Card(
+                                  margin: EdgeInsets.all(10.0),
+                                  child: ListTile(
+                                    leading: Checkbox(
+                                        value: data[index].isDone == 0
+                                            ? false
+                                            : true,
+                                        onChanged: (value) async {
+                                          if (value == false) {
+                                            context
+                                                .read<TodoProvider>()
+                                                .markUnDone(
+                                                    data[index].id ?? 0);
+                                          }
+
+                                          if (value == true) {
+                                            context
+                                                .read<TodoProvider>()
+                                                .markDone(data[index].id ?? 0);
+                                          }
+                                          setState(() {});
+                                        }),
+                                    title: Text(snapshot.data![index].title),
+                                    subtitle:
+                                        Text(snapshot.data![index].description),
+                                    onTap: () {
+                                      int id = data[index].id ?? 0;
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SingleTask(id: id)));
+                                    },
+                                  ),
+                                ),
+                                child: null,
+                              ),
+                            ),
+                          );
+                  }
+                  if (snapshot.hasError) {
+                    return Text('There is an error ${snapshot.error}');
+                  }
+                  return Text('Just here');
+                default:
+                  return ErrorWidget(Exception('An unexpected error occured'));
+              }
+            },
           ),
-          child: null,
         ),
       ],
     );
---
+++ lib/widgets/task_form.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/provider/todo_provider.dart';
+
+import '../model/task.dart';
+import '../service/database.dart';
+
+class TaskForm extends StatefulWidget {
+  TaskForm({super.key});
+
+  @override
+  TaskFormState createState() {
+    return TaskFormState();
+  }
+}
+
+class TaskFormState extends State<TaskForm> {
+  final _formKey = GlobalKey<FormState>();
+  final descriptionController = TextEditingController();
+  final titlecontroller = TextEditingController();
+  final database = MyDatabase();
+
+  @override
+  void dispose() {
+    descriptionController.dispose();
+    titlecontroller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: titlecontroller,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: descriptionController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter the description';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  isDense: true, contentPadding: EdgeInsets.all(40)),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')));
+                  }
+                  String title = titlecontroller.text.toString();
+                  String description = descriptionController.text.toString();
+                  var task =
+                      Task(title: title, description: description, isDone: 1);
+                  context.read<TodoProvider>().addTodo(task);
+                  Navigator
+                },
+                child: const Text('Submit'))
+          ],
+        ));
+  }
+}
